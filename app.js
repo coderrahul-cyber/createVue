5dcf82abf7f61c41f37c5ebd19bb52bee03c8e8b
rahul <rahulsmant299@gmail.com>
2024-04-08 15:27:34 +0530

    just

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// main backend code here
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//
+
 var passport = require('passport');
 var expressSession = require('express-session');
 var flash = require('connect-flash');
