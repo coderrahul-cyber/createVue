cf028d0409dd39c66b7ad641f0007b52924dd6ee
rahul <rahulsmant299@gmail.com>
2024-04-10 22:00:21 +0530

    updated

--- routes/feed.js
+++ routes/feed.js
@@ -8,13 +8,20 @@ const postModel = require('../model/post.model');
 
 router.get('/', async (req, res) => {
     try {
-        const posts = await postModel.find({}).populate('userID' , 'username');
-        res.render('feed', { posts: posts});
+       // mongoose me aggreate is used for to processes the data records and return computed results . like we have different ,methods in array .
+        const randomPosts = await postModel.aggregate([
+            // isse me sample use hum kiya kyuki sample is used to take random records from the data base it size as a object (which means that that number records will take randomly from data base)
+            { $sample: { size: 24 } }
+        ]);
+        
+
+        res.render('feed', { posts: randomPosts });
     } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("Error fetching random posts:", error);
         res.status(500).send("Internal Server Error");
     }
 });
+
 
 
 
